569779d2c618e580047ebad6dd4e435b4204320d
Isabel <isabelpastorlopezcv@gmail.com>
2022-12-10 09:47:17 +0100

    probando

--- js/script.js
+++ js/script.js
@@ -29,13 +29,13 @@
  
    //accede primero a https://cors-anywhere.herokuapp.com/ o https://cors-anywhere.herokuapp.com/corsdemo para habilitarlo de forma temporal
  
-   fetch(/**corsAnywhere +*/ imgURL/** , {
+   fetch(/**corsAnywhere +*/ imgURL, {
      method: "GET",
      headers: new Headers({
        "Content-Type": "application/json",
        "Access-Control-Allow-Origin": "*",
      }),
-   }*/)
+   })
      .then(logStatus)
      .then(checkResponse)
      .then((response) => response.blob())
